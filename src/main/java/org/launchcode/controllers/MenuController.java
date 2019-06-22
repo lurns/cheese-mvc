d8c0d82b03ba6dc7b8233f4d94085d9ae36b6e48
Lurns <lrnlee08@gmail.com>
2019-06-22 12:21:32 -0500

    initialized menu controller and templates

--- src/main/java/org/launchcode/controllers/MenuController.java
+++ src/main/java/org/launchcode/controllers/MenuController.java
@@ -1,10 +1,13 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.Menu;
 import org.launchcode.models.data.CheeseDao;
 import org.launchcode.models.data.MenuDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("menu")
@@ -16,7 +19,21 @@ public class MenuController {
     @Autowired
     private CheeseDao cheeseDao;
 
-    
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    private String index(Model model) {
+        model.addAttribute("title", "Menus");
+        model.addAttribute("menus", menuDao.findAll());
+
+        return "menu/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    private String displayAddForm(Model model) {
+        model.addAttribute("title", "Add Menu");
+        model.addAttribute(new Menu());
+
+        return "menu/add";
+    }
 
 
 }
